c6efdbf23672e45c1359e51631185b1295eaffcd
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-18 12:13:02 +0530

    added @GeneratedValue for id in Todo entity to support adding new todos.

--- src/main/java/com/example/todoapp/model/Todo.java
+++ src/main/java/com/example/todoapp/model/Todo.java
@@ -1,6 +1,8 @@
 package com.example.todoapp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Todo {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String title;
     private boolean completed;
