2ccebf8afcd9e617b058473b58469d57d8b0d00e
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-19 14:10:45 +0530

    replace Todo model with entity

--- src/main/java/com/example/todoapp/entity/Todo.java
+++ src/main/java/com/example/todoapp/entity/Todo.java
@@ -1,4 +1,4 @@
-package com.example.todoapp.model;
+package com.example.todoapp.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/example/todoapp/repository/TodoRepository.java
+++ src/main/java/com/example/todoapp/repository/TodoRepository.java
@@ -1,6 +1,6 @@
 package com.example.todoapp.repository;
 
-import com.example.todoapp.model.Todo;
+import com.example.todoapp.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository <Todo, String> {
